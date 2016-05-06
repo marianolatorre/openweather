bc7489f3cf22cc0ac1ac1f2c132932f39487c675
Mariano Latorre <Mariano.Latorre@ig.com>
2016-05-06 14:05:19 +0100

    Initial Commit

---
+++ weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weather
+//
+//  Created by Mariano Latorre on 5/6/16.
+//  Copyright © 2016 Mariano Latorre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weather
+//
+//  Created by Mariano Latorre on 5/6/16.
+//  Copyright © 2016 Mariano Latorre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
